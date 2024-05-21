80c0b33dbdb66013e0af63f48fd3d378e3d82ace
berseabra <bernardo.seabra@cubos.io>
2024-05-21 15:15:33 -0300

    update categoryTree

--- react/components/CategoryTree.tsx
+++ react/components/CategoryTree.tsx
@@ -1,9 +1,17 @@
-import { Skeleton } from '@vtex/admin-ui'
-import React from 'react'
+import { Button, Checkbox, Skeleton } from '@vtex/admin-ui'
+import React, { useState } from 'react'
 import { useQuery } from 'react-apollo'
 import type { Category, Query } from 'ssesandbox04.catalog-importer'
 
 import CATEGORIES_QUERY from '../graphql/categories.graphql'
+
+interface CheckedCategories {
+  [key: string]: boolean
+}
+
+interface ExpandedCategories {
+  [key: string]: boolean
+}
 
 const convertCategory = (c: Category): Partial<Category> =>
   ({
@@ -17,13 +25,69 @@ const CategoryTree = () => {
     notifyOnNetworkStatusChange: true,
   })
 
+  const [checkedCategories, setCheckedCategories] = useState<CheckedCategories>(
+    {}
+  )
+
+  const [
+    expandedCategories,
+    setExpandedCategories,
+  ] = useState<ExpandedCategories>({})
+
+  const handleCategoryChange = (categoryId: string) => {
+    setCheckedCategories((prevState) => ({
+      ...prevState,
+      [categoryId]: !prevState[categoryId],
+    }))
+  }
+
+  const handleExpandChange = (categoryId: string) => {
+    setExpandedCategories((prevState) => ({
+      ...prevState,
+      [categoryId]: !prevState[categoryId],
+    }))
+  }
+
+  const renderCategory = (category: Partial<Category>, level = 0) => (
+    <div
+      key={category.id}
+      style={{ marginLeft: level * 20, marginBottom: '10px' }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        {category.subCategories && category.subCategories.length > 0 && (
+          <Button
+            onClick={() => handleExpandChange(category.id!)}
+            style={{ cursor: 'pointer', marginRight: '5px' }}
+          >
+            {expandedCategories[category.id!] ? ' ↓' : '>'}
+          </Button>
+        )}
+        <Checkbox
+          checked={!!checkedCategories[category.id!]}
+          label={category.name}
+          onChange={() => handleCategoryChange(category.id!)}
+        />
+      </div>
+      {expandedCategories[category.id!] &&
+        category.subCategories &&
+        category.subCategories.map((child: any) =>
+          renderCategory(child, level + 1)
+        )}
+    </div>
+  )
+
   const categories = data?.categories?.map(convertCategory)
 
   return (
-    <div>
+    <div className="bg-muted-5 pa8">
       {error && <pre>{JSON.stringify(error, null, 2)}</pre>}
       {loading && <Skeleton style={{ height: 300 }} />}
-      {!loading && <pre>{JSON.stringify(categories, null, 2)}</pre>}
+      {!loading &&
+        categories &&
+        categories.map((category: any) => renderCategory(category))}
+      <div style={{ marginTop: '10px' }}>
+        <Button style={{ marginTop: '10px' }}>Iniciar processamento</Button>
+      </div>
     </div>
   )
 }
