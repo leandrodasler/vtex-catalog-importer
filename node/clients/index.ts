ddeab77d0bb06ff09bda36d49088594c68db98a0
Tiago de Andrade Freire <tiago.freire@cubos.io>
2024-06-09 14:42:37 -0300

    refactor: node global typings on clients

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,12 +1,28 @@
-import type { Cached, ClientsConfig } from '@vtex/api'
+import type {
+  Cached,
+  ClientsConfig,
+  RecorderState,
+  ServiceContext,
+} from '@vtex/api'
 import { IOClients, LRUCache } from '@vtex/api'
+import type { AppSettingsInput } from 'ssesandbox04.catalog-importer'
 
 import HttpClient from './HttpClient'
 
-export class Clients extends IOClients {
+class Clients extends IOClients {
   public get httpClient() {
     return this.getOrSet('httpClient', HttpClient)
   }
+}
+
+declare global {
+  interface State extends RecorderState {
+    body: {
+      settings?: AppSettingsInput
+    }
+  }
+
+  type Context = ServiceContext<Clients, State>
 }
 
 const memoryCache = new LRUCache<string, Cached>({ max: 5000 })
--- node/index.ts
+++ node/index.ts
@@ -1,21 +1,8 @@
-import type { RecorderState, ServiceContext } from '@vtex/api'
 import { Service } from '@vtex/api'
-import type { AppSettingsInput } from 'ssesandbox04.catalog-importer'
 
-import type { Clients } from './clients'
 import clients from './clients'
 import schemaDirectives from './directives'
 import resolvers from './resolvers'
-
-declare global {
-  interface State extends RecorderState {
-    body: {
-      settings?: AppSettingsInput
-    }
-  }
-
-  type Context = ServiceContext<Clients, State>
-}
 
 export default new Service({
   clients,
