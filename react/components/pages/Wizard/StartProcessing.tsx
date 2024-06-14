d657c4bc5ac891e7e1a2e96457a4f77806857880
berseabra <bernardo.seabra@cubos.io>
2024-06-14 15:38:48 -0300

    update style category Tree

--- react/components/pages/Wizard/StartProcessing.tsx
+++ react/components/pages/Wizard/StartProcessing.tsx
@@ -36,15 +36,14 @@ interface StartProcessingProps {
   settings: AppSettingsInput
 }
 
-const StartProcessing = ({
+const StartProcessing: React.FC<StartProcessingProps> = ({
   checkedTreeOptions,
   optionsChecked,
   state,
   settings,
-}: StartProcessingProps) => {
+}) => {
   const { formatMessage } = useIntl()
   const showToast = useToast()
-
   const [executeImport, { loading }] = useMutation<
     Mutation,
     MutationExecuteImportArgs
@@ -72,29 +71,57 @@ const StartProcessing = ({
   const convertChildrenToObject = (
     children: Category[]
   ): { [key: string]: Category & { checked: boolean } } => {
-    return children.reduce((acc, child) => {
-      acc[child.id] = { ...child, checked: true }
-
-      return acc
-    }, {} as { [key: string]: Category & { checked: boolean } })
+    return children.reduce(
+      (acc, child) => ({ ...acc, [child.id]: { ...child, checked: true } }),
+      {} as { [key: string]: Category & { checked: boolean } }
+    )
   }
 
   const renderTree = (
     tree: { [key: string]: Category & { checked: boolean } },
     level = 0
-  ) => {
-    return (
-      <ul style={{ paddingLeft: level * 20 }}>
-        {Object.entries(tree).map(([key, value]) => (
-          <li key={key}>
-            <span>{value.name}</span>
-            {value.children &&
-              value.children.length > 0 &&
-              renderTree(convertChildrenToObject(value.children), level + 1)}
-          </li>
-        ))}
-      </ul>
-    )
+  ) => (
+    <ul style={{ paddingLeft: '1rem' }}>
+      {Object.entries(tree).map(([key, value]) => (
+        <li key={key} style={{ marginLeft: '1rem' }}>
+          <span>{value.name}</span>
+          {value.children &&
+            value.children.length > 0 &&
+            renderTree(convertChildrenToObject(value.children), level + 1)}
+        </li>
+      ))}
+    </ul>
+  )
+
+  const renderOption = (label: string, condition: boolean) => (
+    <div>
+      {label}:
+      {condition
+        ? formatMessage(messages.yesLabel)
+        : formatMessage(messages.noLabel)}
+    </div>
+  )
+
+  const handleImport = () => {
+    executeImport({
+      variables: {
+        args: {
+          categoryTree: Object.entries(
+            checkedTreeOptions
+          ).map(([id, { name }]) => ({ id, name })),
+          settings,
+          importImages: optionsChecked.checkedItems.includes(
+            IMPORT_OPTIONS.IMPORT_IMAGE
+          ),
+          importPrices: optionsChecked.checkedItems.includes(
+            IMPORT_OPTIONS.IMPORT_PRICE
+          ),
+          stocksOption: optionsChecked.stockOption,
+          ...(optionsChecked.stockOption === STOCK_OPTIONS.TO_BE_DEFINED &&
+            optionsChecked.value && { stockValue: +optionsChecked.value }),
+        },
+      },
+    })
   }
 
   return (
@@ -102,29 +129,23 @@ const StartProcessing = ({
       <h3>{formatMessage(messages.optionsCategories)}</h3>
       {renderTree(checkedTreeOptions)}
       <h3>{formatMessage(messages.optionsLabel)}</h3>
-      <div>
-        {formatMessage(messages.importImage)}:{' '}
-        {optionsChecked.checkedItems.includes(IMPORT_OPTIONS.IMPORT_IMAGE)
-          ? formatMessage(messages.yesLabel)
-          : formatMessage(messages.noLabel)}
-      </div>
-      <div>
-        {formatMessage(messages.importPrice)}:{' '}
-        {optionsChecked.checkedItems.includes(IMPORT_OPTIONS.IMPORT_PRICE)
-          ? formatMessage(messages.yesLabel)
-          : formatMessage(messages.noLabel)}
-      </div>
+      {renderOption(
+        formatMessage(messages.importImage),
+        optionsChecked.checkedItems.includes(IMPORT_OPTIONS.IMPORT_IMAGE)
+      )}
+      {renderOption(
+        formatMessage(messages.importPrice),
+        optionsChecked.checkedItems.includes(IMPORT_OPTIONS.IMPORT_PRICE)
+      )}
       <div>
         {formatMessage(messages.importStocks)}:{' '}
-        {optionsChecked.stockOption === STOCK_OPTIONS.KEEP_SOURCE ? (
-          formatMessage(messages.optionsSource)
-        ) : optionsChecked.stockOption === STOCK_OPTIONS.UNLIMITED ? (
-          formatMessage(messages.optionsUnlimited)
-        ) : (
-          <>
-            {formatMessage(messages.optionsDefined)}: {optionsChecked.value}
-          </>
-        )}
+        {optionsChecked.stockOption === STOCK_OPTIONS.KEEP_SOURCE
+          ? formatMessage(messages.optionsSource)
+          : optionsChecked.stockOption === STOCK_OPTIONS.UNLIMITED
+          ? formatMessage(messages.optionsUnlimited)
+          : `${formatMessage(messages.optionsDefined)}: ${
+              optionsChecked.value
+            }`}
       </div>
       <Flex justify="space-between" className={csx({ marginTop: '$space-4' })}>
         <Button
@@ -138,31 +159,7 @@ const StartProcessing = ({
           icon={<IconArrowLineDown />}
           disabled={loading}
           loading={loading}
-          onClick={() => {
-            executeImport({
-              variables: {
-                args: {
-                  categoryTree: Object.entries(checkedTreeOptions).map((c) => ({
-                    id: c[0],
-                    name: c[1].name,
-                  })),
-                  settings,
-                  importImages: optionsChecked.checkedItems.includes(
-                    IMPORT_OPTIONS.IMPORT_IMAGE
-                  ),
-                  importPrices: optionsChecked.checkedItems.includes(
-                    IMPORT_OPTIONS.IMPORT_PRICE
-                  ),
-                  stocksOption: optionsChecked.stockOption,
-                  ...(optionsChecked.stockOption ===
-                    STOCK_OPTIONS.TO_BE_DEFINED &&
-                    optionsChecked.value && {
-                      stockValue: +optionsChecked.value,
-                    }),
-                },
-              },
-            })
-          }}
+          onClick={handleImport}
         >
           {formatMessage(messages.startLabel)}
         </Button>
